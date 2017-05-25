50c857df8fa3c136476f163b3f8bce43e8ae8ee4
Ben King <ben@designbybenking.co.uk>
2017-05-26 00:13:18 +0100

    Fix Flickr’s broken JSON, add Shoebox

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,11 +6,23 @@ export default Ember.Route.extend({
     ajax: Ember.inject.service(),
     model() {
         const url = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json';
+        const shoebox = this.get('fastboot.shoebox');
+        const shoeboxStore = shoebox.retrieve('photos');
+
         if (this.get('fastboot.isFastBoot')) {
             return fetch(`${url}&nojsoncallback=1`)
                 .then((response) => {
-                    return response.json();
+                    return response.text();
+                }).then((text) => {
+                    const fixed = text.replace(/\\'/g, "&#39;");
+                    const json = JSON.parse(fixed);
+                    shoebox.put('photos', json);
+                    return json;
                 });
+        }
+
+        if (shoeboxStore) {
+            return shoeboxStore;
         }
 
         return this.get('ajax').request(url, {
